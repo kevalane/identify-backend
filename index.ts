6cc4c927a74099378f5b8360939a9919437c8796
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-03-09 16:38:41 +0100

    generic endpoint working

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 
 const app = express();
 
@@ -6,4 +6,8 @@ const port = 3000;
 
 app.listen(port, () => {
 	console.log('Running on port ' + port);
+});
+
+app.get('/', (req: Request, res: Response) => {
+	return res.status(200).send({msg: 'Hello world'});
 });
