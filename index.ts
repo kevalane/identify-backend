7fc3e671596344d7a58e8d61ab7f5c056a460e5a
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-03-09 17:18:05 +0100

    mvc outlined

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
 import express, {Request, Response} from 'express';
 
+import {authRouter} from './routes/auth.routes';
+
+// global constants
+const configUrl: string = '/api/';
+
+// init app
 const app = express();
-
 const port = 3000;
-
 app.listen(port, () => {
 	console.log('Running on port ' + port);
 });
+
+// Routes
+app.use(configUrl + 'auth', authRouter);
 
 app.get('/', (req: Request, res: Response) => {
 	return res.status(200).send({msg: 'Hello world'});
