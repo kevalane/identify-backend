dd3370cf892d94d1bee2ef17d1c04eca10c9cbe9
kevalane <kevin@jkmholding.com>
2022-03-15 21:03:58 +0100

    implemented auth routes

---
+++ routes/auth.routes.ts
@@ -0,0 +1,8 @@
+import express, {Router} from 'express';
+import {initSign} from '../controllers/auth.controller';
+
+let authRouter: Router = express.Router();
+
+authRouter.get('/initSign', initSign);
+
+export { authRouter };
