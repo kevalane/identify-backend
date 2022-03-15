4d9092eefed19d3c50f58fa969f62b8abb887c91
kevalane <kevin@jkmholding.com>
2022-03-15 21:34:34 +0100

    added imports to auth controller

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,4 +1,11 @@
 import express, {Request, Response} from 'express';
+const request = require('request');
+
+// global vars
+const configUrl: string | undefined = process.env.URL;
+const creditor_id: string | undefined = process.env.CREDITOR_ID;
+const api_key: string | undefined = process.env.API_KEY;
+
 
 function initSign(req: Request, res: Response): Response {
     return res.send({msg: 'initSign hit'});
