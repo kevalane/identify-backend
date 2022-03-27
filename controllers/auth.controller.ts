d2f989285dfd9ab7b15a18d6c40b528e932d8650
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-03-27 22:24:27 +0200

    setup axios with cert

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,4 +1,27 @@
 import express, {Request, Response} from 'express';
+import axiosLib, { Axios } from 'axios';
+import https from 'https';
+import fs from 'fs';
+
+// configs
+const config: any = {
+    mobileBankIdPolicy: '1.2.3.4.25',
+    bankdIdUrl: 'https://appapi2.test.bankid.com/rp/v5',
+    pfx: fs.readFileSync('./cert/FPTestcert3_20200618.p12'),
+    passphrase: 'qwerty123',
+    ca: fs.readFileSync('./cert/public-test-key.crt'),
+}
+
+const axios: Axios = axiosLib.create({
+    httpsAgent: new https.Agent({
+        pfx: config.pfx,
+        passphrase: config.passphrase,
+        ca: config.ca,
+    }),
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
 
 function initSign(req: Request, res: Response): Response {
     return res.send({msg: 'initSign hit'});
